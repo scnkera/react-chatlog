c4663cdad0f1a401caa81baba471c6b68a9b3518
Christelle Nkera <scnkera@gmail.com>
2024-12-16 11:59:07 -0500

    Changed title

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Vladmir and Estragon</h1>
       </header>
       <ChatLog entries={messages}/>
     </div>
